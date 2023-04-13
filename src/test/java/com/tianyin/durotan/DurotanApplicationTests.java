03350c8be4df3990cc1eaa67ab28f4fcb49fe9ec
Tianyin Zhang <tianyinzhang@Tianyindebijibendiannao.local>
2023-04-13 15:00:55 +0800

    初始化项目

---
+++ src/test/java/com/tianyin/durotan/DurotanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianyin.durotan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurotanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
